5c6f5c120bad664fe439cb3a3a0a16cdc8f103f5
Michael Binz <michab66@gmail.com>
2019-12-12 21:31:58 +0100

    Fixed memory allocation failure.

--- jlgen/jlgen.cpp
+++ jlgen/jlgen.cpp
@@ -82,15 +82,20 @@ static void UpdateIcon(LPCTSTR iconFile, unsigned int iconIndex, unsigned int re
     DWORD dwBytesRead;
 
     // Read the Reserved word
-    ReadFile(hFile, &(pIconDir->idReserved), sizeof(WORD), &dwBytesRead, NULL);
+    WORD tmpReserved;
+    ReadFile(hFile, &tmpReserved, sizeof(WORD), &dwBytesRead, NULL);
     // Read the Type word - make sure it is 1 for icons
-    ReadFile(hFile, &(pIconDir->idType), sizeof(WORD), &dwBytesRead, NULL);
+    WORD tmpIdType;
+    ReadFile(hFile, &tmpIdType, sizeof(WORD), &dwBytesRead, NULL);
     // Read the count - how many images in this file?
-    ReadFile(hFile, &(pIconDir->idCount), sizeof(WORD), &dwBytesRead, NULL);
-    // Reallocate IconDir so that idEntries has enough room for idCount elements
-    pIconDir = (LPICONDIR)realloc(
-        pIconDir,
-        (sizeof(WORD) * 3) + (sizeof(ICONDIRENTRY) * pIconDir->idCount));
+    WORD tmpIdCount;
+    ReadFile(hFile, &tmpIdCount, sizeof(WORD), &dwBytesRead, NULL);
+    // Allocate IconDir so that idEntries has enough room for idCount elements
+    pIconDir = (LPICONDIR)malloc(
+        sizeof(ICONDIR) + ((tmpIdCount - 1) * sizeof(ICONDIRENTRY) ));
+    pIconDir->idReserved = 0;
+    pIconDir->idType = tmpIdType;
+    pIconDir->idCount = tmpIdCount;
     // Read the ICONDIRENTRY elements
     ReadFile(
         hFile,
@@ -98,7 +103,7 @@ static void UpdateIcon(LPCTSTR iconFile, unsigned int iconIndex, unsigned int re
         pIconDir->idCount * sizeof(ICONDIRENTRY),
         &dwBytesRead, 
         NULL);
-    free(pIconDir);
+
     // Loop through and read in each image
     for (int i = 0; i < pIconDir->idCount; i++)
     {
@@ -213,95 +218,6 @@ static void UpdateIcon(LPCTSTR iconFile, unsigned int iconIndex, unsigned int re
     // Clean up the ICONDIR memory
     free(pIconDir);
 }
-
-//static void ErrorHandler( std::wstring msg )
-//{
-//    std::wcerr << msg << std::endl;
-//
-//    std::exit(1);
-//}
-//
-//static void generate()
-//{
-//    std::exit(0);
-//
-//    HGLOBAL hResLoad;   // handle to loaded resource
-//    HMODULE hExe;       // handle to existing .EXE file
-//    HRSRC hRes;         // handle/ptr. to res. info. in hExe
-//    HANDLE hUpdateRes;  // update resource handle
-//    LPVOID lpResLock;   // pointer to resource data
-//    BOOL result;
-//#define IDD_HAND_ABOUTBOX   103
-//#define IDD_FOOT_ABOUTBOX   110
-//
-//    // Load the .EXE file that contains the dialog box you want to copy.
-//    hExe = LoadLibrary(TEXT("hand.exe"));
-//    if (hExe == NULL)
-//    {
-//        ErrorHandler(TEXT("Could not load exe."));
-//        return;
-//    }
-//
-//    // Locate the dialog box resource in the .EXE file.
-//    hRes = FindResource(hExe, MAKEINTRESOURCE(IDD_HAND_ABOUTBOX), RT_DIALOG);
-//    if (hRes == NULL)
-//    {
-//        ErrorHandler(TEXT("Could not locate dialog box."));
-//        return;
-//    }
-//
-//    // Load the dialog box into global memory.
-//    hResLoad = LoadResource(hExe, hRes);
-//    if (hResLoad == NULL)
-//    {
-//        ErrorHandler(TEXT("Could not load dialog box."));
-//        return;
-//    }
-//
-//    // Lock the dialog box into global memory.
-//    lpResLock = LockResource(hResLoad);
-//    if (lpResLock == NULL)
-//    {
-//        ErrorHandler(TEXT("Could not lock dialog box."));
-//        return;
-//    }
-//
-//    // Open the file to which you want to add the dialog box resource.
-//    hUpdateRes = BeginUpdateResource(TEXT("foot.exe"), FALSE);
-//    if (hUpdateRes == NULL)
-//    {
-//        ErrorHandler(TEXT("Could not open file for writing."));
-//        return;
-//    }
-//
-//    // Add the dialog box resource to the update list.
-//    result = UpdateResource(hUpdateRes,    // update resource handle
-//        RT_DIALOG,                         // change dialog box resource
-//        MAKEINTRESOURCE(IDD_FOOT_ABOUTBOX),         // dialog box id
-//        MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL),  // neutral language
-//        lpResLock,                         // ptr to resource info
-//        SizeofResource(hExe, hRes));       // size of resource info
-//
-//    if (result == FALSE)
-//    {
-//        ErrorHandler(TEXT("Could not add resource."));
-//        return;
-//    }
-//
-//    // Write changes to FOOT.EXE and then close it.
-//    if (!EndUpdateResource(hUpdateRes, FALSE))
-//    {
-//        ErrorHandler(TEXT("Could not write changes to file."));
-//        return;
-//    }
-//
-//    // Clean up.
-//    if (!FreeLibrary(hExe))
-//    {
-//        ErrorHandler(TEXT("Could not free executable."));
-//        return;
-//    }
-//}
 
 int wmain( int argc, wchar_t** argv )
 {
