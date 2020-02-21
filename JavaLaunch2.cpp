e43355b4e80b97a780774a2f72fb99583b31f903
Michael Binz <michab66@gmail.com>
2020-02-21 19:18:08 +0100

    Streamlined icon code.  The end is near.

--- JavaLaunch2.cpp
+++ JavaLaunch2.cpp
@@ -6,8 +6,7 @@
  * LGPL
  */
 
-#include <array>
-
+// Use extendedGDI+.
 #define GDIPVER     0x0110
 
 #include <tchar.h>
@@ -17,324 +16,200 @@
 #include "Shlwapi.h"
 
 #include <jni.h>
+
 #include <iostream>
 #include <string>
 #include <map>
+#include <sstream>
 
 // Get our resource definitions.
 #include "resource.h"
 
-static int dieWithMessage( const TCHAR* msg )
-{
-    TCHAR szExeFileName[MAX_PATH];
-    GetModuleFileName(NULL, szExeFileName, MAX_PATH);
-
-    MessageBox(NULL, msg, szExeFileName, MB_OK | MB_APPLMODAL);
-
-    return 1;
-}
-
-/**
- * Read a string resource for a resource id.
- *
- * @param instance The application's instance handle.
- * @param id The resource id.
- * @return The resource string converted to 8bit platform encoding. The 
- * string is empty if the resource was not found.
- */
-static std::string getStringResource( HINSTANCE instance, UINT id )
-{
-    WCHAR* notUsed{};
-
-    // Get the size of the configured resource string.
-    int rcw = LoadStringW(
-        instance, 
-        id,
-        // We do not use this since it points to the wide character
-        // version of the resource.
-        (WCHAR*)(&notUsed), 
-        0);
-
-    // Create our result string in the required size.
-    std::string result(rcw , '\0');
-
-    // LoadStringA gets the actual result converted into the
-    // 8bit platform encoding.
-    int rca = LoadStringA(
-        instance, 
-        id,
-        // We directly write into the result string.
-        &result[0],
-        rcw+1 );
-
-    if (rcw == rca)
-        return result;
-
-    // TODO(micbinz) implement the error path.
-    DWORD error = 
-        GetLastError();
-
-    // Return the empty string in case of an error.
-    return std::string{};
-}
-
-/*
- * Windows entry point.
- */
-int APIENTRY micbinzwWinMain(
-    _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-    UNREFERENCED_PARAMETER(nCmdShow);
-
-    std::string mainModule = getStringResource(
-        hInstance, 
-        IDS_JAVA_MAIN_MODULE );
-
-    if ( mainModule.empty() )
-        return dieWithMessage(_T("IDS_JAVA_MAIN_MODULE not in resources."));
-    // Add the reqired option prefix to set the main module.
-    mainModule = 
-        "--add-modules=" + 
-        mainModule;
-
-    std::string mainClassName = getStringResource(
-        hInstance,
-        IDS_JAVA_MAIN_CLASS);
-    if ( mainClassName.empty() )
-        return dieWithMessage(_T("IDS_JAVA_MAIN_CLASS not in resources."));
-
-    std::array<JavaVMOption,1>jvmopt{ 
-        const_cast<char*>(mainModule.c_str()),
-        nullptr 
-    };
-
-    JavaVMInitArgs vmArgs{
-        JNI_VERSION_1_8,
-        static_cast<jint>(jvmopt.size()),
-        jvmopt.data(),
-        JNI_FALSE 
-    };
-
-    // Create the JVM.
-    JavaVM* javaVM = nullptr;
-    JNIEnv* env = nullptr;
-    long rc = JNI_CreateJavaVM(
-        &javaVM,
-        (void**)&env, 
-        &vmArgs );
-    if (rc == JNI_ERR) {        
-        return dieWithMessage( _T("Error creating VM\n. Exiting ...") );
-    }
-
-    // TODO(michab) Decide if we keep this.
-    // Reads a system property.
-    //jclass jcls = env->FindClass("java/lang/System");
-    //if (jcls == NULL) {
-    //    env->ExceptionDescribe();
-    //    javaVM->DestroyJavaVM();
-    //    return 1;
-    //}
-    //if (jcls != NULL) {
-    //    jmethodID methodId = env->GetStaticMethodID(
-    //        jcls,
-    //        "getProperty",
-    //        "(Ljava/lang/String;)Ljava/lang/String;");
-    //    if (methodId != NULL) {
-    //        jstring str = env->NewStringUTF("java.home");
-    //        jstring got = (jstring) env->CallStaticObjectMethod(jcls, methodId, str);
-    //        if (env->ExceptionCheck()) {
-    //            env->ExceptionDescribe();
-    //            env->ExceptionClear();
-    //        }
-    //        const char* cstr = env->GetStringUTFChars(got, 0);
-    //        std::cout << cstr << std::endl;
-    //        env->ReleaseStringUTFChars(got, cstr);
-    //        env->DeleteLocalRef(got);        
-    //    }
-    //}
-
-    jclass stringClass = 
-        env->FindClass("java/lang/String");
-    jclass mainClass = 
-        env->FindClass(mainClassName.c_str());
-    if ( ! mainClass )
-        return dieWithMessage(_T("IDS_JAVA_MAIN_CLASS not found."));
-
-    jmethodID mainMethod = env->GetStaticMethodID(
-        mainClass,
-        "main",
-        "([Ljava/lang/String;)V");
-    if ( ! mainMethod )
-        return dieWithMessage(_T("No main() found."));
-
-    jobjectArray argv = 
-        env->NewObjectArray(0,stringClass,0);
-
-    env->CallStaticObjectMethod(
-        mainClass,
-        mainMethod,
-        argv);
-    if (env->ExceptionCheck()) {
-        env->ExceptionDescribe();
-        env->ExceptionClear();
-        return dieWithMessage(_T("No good."));
-    }
-
-    // Waits until the VM terminates.
-    javaVM->DestroyJavaVM();
-
-    return 0;
-}
 #pragma comment(lib, "gdiplus.lib")
 #pragma comment(lib, "Shlwapi.lib")
 
-// Internet hacked.  The original code is from microsoft.
-// Switch to MS solution.
-// https://docs.microsoft.com/en-us/windows/win32/gdiplus/-gdiplus-retrieving-the-class-identifier-for-an-encoder-use
-static size_t GetImageEncoder(const wchar_t* form, CLSID* clsID)
+namespace
 {
-    size_t nRet = -1;
-    unsigned int encodersCount = 0;
-    unsigned int encodersSize = 0;
-    Gdiplus::ImageCodecInfo* imageCodecs = nullptr;
-
-    Gdiplus::GetImageEncodersSize(&encodersCount, &encodersSize);
-
-    imageCodecs = (Gdiplus::ImageCodecInfo*)malloc(encodersSize);
-
-    Gdiplus::GetImageEncoders(encodersCount, encodersSize, imageCodecs);
-    for (size_t index = 0; index < encodersCount; index++)
+    std::wstring GetSuffix(std::wstring& path)
     {
-        if (wcscmp(imageCodecs[index].MimeType, form) == 0)
-        {
-            Gdiplus::ImageCodecInfo* current = &imageCodecs[index];
-            *clsID = imageCodecs[index].Clsid;
-            nRet = index;
-            break;
-        }
+        auto lastIdx = path.find_last_of(L".");
+        if (lastIdx == std::wstring::npos)
+            return std::wstring{};
+
+        return path.substr(lastIdx);
     }
 
-    if (imageCodecs)
-        free(imageCodecs);
-    return nRet;
-}
-
-/**
- * Compute the passed bitmap's mime type.
- */
-static std::wstring GetMimeType(Gdiplus::Bitmap* bitmap)
-{
-    GUID raw;
-    bitmap->GetRawFormat(&raw);
-
-    UINT numDecoders;
-    UINT size;
-    Gdiplus::GetImageDecodersSize( &numDecoders, &size );
-    
-    std::unique_ptr<uint8_t[]> buffer(new uint8_t[size]);
-
-    Gdiplus::GetImageDecoders(
-        numDecoders, 
-        size,
-        (Gdiplus::ImageCodecInfo*)&buffer[0]);
-
-    Gdiplus::ImageCodecInfo* tbuffer = 
-        (Gdiplus::ImageCodecInfo*) &buffer[0];
-
-    for (size_t i = 0; i < numDecoders; ++i)
+    std::wstring GetPath(std::wstring& path)
     {
-        Gdiplus::ImageCodecInfo* current = &tbuffer[i];
+        auto lastIdx = path.find_last_of(L".");
+        if (lastIdx == std::wstring::npos)
+            return std::wstring{};
 
-        std::wcout << i << " : " << current->MimeType << std::endl;
-
-        if (current->FormatID == raw)
-        {
-            std::wstring result = current->MimeType;
-            return result;
-        }
+        return path.substr(0,lastIdx);
     }
 
-    std::wstring empty;
-    return empty;
-}
+    /**
+     * Compute the passed bitmap's mime type.
+     */
+    std::wstring GetMimeType(Gdiplus::Bitmap* bitmap)
+    {
+        GUID raw;
+        bitmap->GetRawFormat(&raw);
 
-static void writeImage(
-    std::wstring strdup,
-    uint32_t width, 
-    uint32_t height,
-    Gdiplus::Bitmap* bitMap)
-{
-    std::map<std::wstring, std::wstring> m_mtMap;
-    m_mtMap[L".jpeg"] = L"image/jpeg";
-    m_mtMap[L".jpe"] = L"image/jpeg";
-    m_mtMap[L".jpg"] = L"image/jpeg";
-    m_mtMap[L".png"] = L"image/png";
-    m_mtMap[L".gif"] = L"image/gif";
-    m_mtMap[L".tiff"] = L"image/tiff";
-    m_mtMap[L".tif"] = L"image/tiff";
-    m_mtMap[L".bmp"] = L"image/bmp";
+        UINT numDecoders;
+        UINT size;
+        Gdiplus::GetImageDecodersSize(&numDecoders, &size);
 
-    Gdiplus::Bitmap* sqeezed  = new Gdiplus::Bitmap(128, 128, PixelFormat32bppARGB);
+        std::unique_ptr<uint8_t[]> buffer(new uint8_t[size]);
 
-    Gdiplus::Graphics* graphic = Gdiplus::Graphics::FromImage(sqeezed);
-    graphic->SetCompositingQuality(Gdiplus::CompositingQuality::CompositingQualityHighQuality);
-    graphic->SetInterpolationMode(Gdiplus::InterpolationMode::InterpolationModeHighQualityBilinear);
-    graphic->SetSmoothingMode(Gdiplus::SmoothingMode::SmoothingModeHighQuality);
-    graphic->DrawImage(bitMap, 0, 0, 128, 128);
-    CLSID encoderCLSID;
-    GetImageEncoder(m_mtMap[L".jpg"].c_str(), &encoderCLSID);
-    sqeezed->Save(strdup.c_str(), &encoderCLSID);
+        Gdiplus::GetImageDecoders(
+            numDecoders,
+            size,
+            (Gdiplus::ImageCodecInfo*) & buffer[0]);
 
-    delete sqeezed;
-}
+        Gdiplus::ImageCodecInfo* tbuffer =
+            (Gdiplus::ImageCodecInfo*) & buffer[0];
+
+        for (size_t i = 0; i < numDecoders; ++i)
+        {
+            Gdiplus::ImageCodecInfo* current = &tbuffer[i];
+
+            std::wcout << i << " : " << current->MimeType << std::endl;
+
+            if (current->FormatID == raw)
+            {
+                std::wstring result = current->MimeType;
+                return result;
+            }
+        }
+
+        std::wstring empty;
+        return empty;
+    }
+
+    /**
+     * Compute the passed bitmap's Clsid.
+     */
+    CLSID GetClsid(Gdiplus::Bitmap* bitmap)
+    {
+        GUID raw;
+        bitmap->GetRawFormat(&raw);
+
+        UINT numDecoders;
+        UINT size;
+        Gdiplus::GetImageDecodersSize(&numDecoders, &size);
+
+        std::unique_ptr<uint8_t[]> buffer(new uint8_t[size]);
+
+        Gdiplus::GetImageDecoders(
+            numDecoders,
+            size,
+            (Gdiplus::ImageCodecInfo*) & buffer[0]);
+
+        Gdiplus::ImageCodecInfo* tbuffer =
+            (Gdiplus::ImageCodecInfo*) & buffer[0];
+
+        for (size_t i = 0; i < numDecoders; ++i)
+        {
+            Gdiplus::ImageCodecInfo* c =
+                &tbuffer[i];
+
+            std::wcout <<
+                i <<
+                " : "
+                << c->MimeType
+                << std::endl;
+
+            if (IsEqualCLSID(c->FormatID, raw))
+                return c->Clsid;
+        }
+
+        return CLSID_NULL;
+    }
+
+    void writeImageFile(
+        std::wstring name,
+        std::wstring suffix,
+        uint32_t dimension,
+        CLSID& clsid,
+        Gdiplus::Bitmap* bitMap)
+    {
+        Gdiplus::Bitmap* sqeezed = 
+            new Gdiplus::Bitmap(
+                dimension,
+                dimension,
+                PixelFormat32bppARGB);
+
+        Gdiplus::Graphics* graphic = 
+            Gdiplus::Graphics::FromImage(
+                sqeezed);
+        graphic->SetCompositingQuality(
+            Gdiplus::CompositingQuality::CompositingQualityHighQuality);
+        graphic->SetInterpolationMode(
+            Gdiplus::InterpolationMode::InterpolationModeHighQualityBilinear);
+        graphic->SetSmoothingMode(
+            Gdiplus::SmoothingMode::SmoothingModeHighQuality);
+        graphic->DrawImage(
+            bitMap,
+            0,
+            0,
+            dimension,
+            dimension);
+
+        std::wostringstream collector;
+        collector <<
+            name <<
+            L"-" <<
+            dimension <<
+            suffix;
+
+        sqeezed->Save(
+            collector.str().c_str(), 
+            &clsid);
+
+        delete sqeezed;
+    }
+
+} // unnamed namespace.
 
 // https://devblogs.microsoft.com/oldnewthing/20101022-00/?p=12473
 // https://stackoverflow.com/questions/51383896/c-gdibitmap-to-png-image-in-memory
 
 int wmain(int argc, _TCHAR* argv[])
 {
+    using std::wstring;
+
     Gdiplus::GdiplusStartupInputEx gdiStartupInput;
     ULONG_PTR gdiplustoken;
-    Gdiplus::GdiplusStartup(&gdiplustoken, &gdiStartupInput, NULL);
+    Gdiplus::GdiplusStartup(&gdiplustoken, &gdiStartupInput, nullptr);
 
-    std::wstring strfilePath = L"mmt-icon-1024.png";
-    std::wstring strdup = L"mmt-icon-128.png";
-    Gdiplus::Bitmap* bitMap = new Gdiplus::Bitmap(strfilePath.c_str());
+    std::wstring strfilePath =
+        L"mmt-icon-1024.png";
+    std::wstring strdup =
+        L"mmt-icon-128.png";
 
-    TCHAR currentDir[1024];
-
-    GetCurrentDirectory(
-        sizeof currentDir,
-        currentDir
-    );
-
-    BOOL exists = PathFileExistsW(strfilePath.c_str() );
-    if (!exists)
+    if (! PathFileExistsW(strfilePath.c_str()))
+        // File not found.
         return 1;
 
-    std::wstring mimeType = GetMimeType(bitMap);
+    Gdiplus::Bitmap* bitMap =
+        new Gdiplus::Bitmap(strfilePath.c_str());
 
-    std::wcout << "micbinz: guid=" << mimeType << std::endl;
+    CLSID fileClsid =
+        GetClsid(bitMap);
+    if (IsEqualCLSID(fileClsid, CLSID_NULL))
+        // Unknown file type.
+        return 1;
 
-//    Gdiplus::Bitmap* sqeezed = new Gdiplus::Bitmap(128, 128, PixelFormat32bppRGB);
-    Gdiplus::Bitmap* sqeezed = new Gdiplus::Bitmap(128, 128, PixelFormat32bppRGB);
-
-    Gdiplus::Graphics* graphic = Gdiplus::Graphics::FromImage(sqeezed);
-    graphic->SetCompositingQuality(Gdiplus::CompositingQuality::CompositingQualityHighQuality);
-    graphic->SetInterpolationMode(Gdiplus::InterpolationMode::InterpolationModeBilinear);
-    graphic->SetSmoothingMode(Gdiplus::SmoothingMode::SmoothingModeHighQuality);
-    graphic->DrawImage(bitMap, 0, 0, 128, 128);
-    CLSID encoderCLSID;
-    GetImageEncoder(mimeType.c_str(), &encoderCLSID);
-    sqeezed->Save(strdup.c_str(), &encoderCLSID);
-
-    delete bitMap;
-    delete sqeezed;
+    wstring baseName =
+        GetPath(strfilePath);
+    wstring suffix =
+        GetSuffix(strfilePath);
+    writeImageFile(baseName, suffix,  16, fileClsid, bitMap);
+    writeImageFile(baseName, suffix,  32, fileClsid, bitMap);
+    writeImageFile(baseName, suffix,  64, fileClsid, bitMap);
+    writeImageFile(baseName, suffix, 128, fileClsid, bitMap);
+    writeImageFile(baseName, suffix, 256, fileClsid, bitMap);
 
     Gdiplus::GdiplusShutdown(gdiplustoken);
     return 0;
