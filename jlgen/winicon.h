adb8c73559cf02bb4a9ea7fe1e5b042418b129a3
Michael Binz <michab66@gmail.com>
2020-02-29 16:15:04 +0100

    Additional documentation.

--- jlgen/winicon.h
+++ jlgen/winicon.h
@@ -1,8 +1,16 @@
 /*
  * Copyright Microsoft
  *
- * See https://devblogs.microsoft.com/oldnewthing/20120720-00/?p=7083
- * See https://devblogs.microsoft.com/oldnewthing/20101022-00/?p=12473
+ * See The format of icon resources 
+ * https://devblogs.microsoft.com/oldnewthing/20120720-00/?p=7083
+ * See Part 1 - Monochrome beginnings
+ * https://devblogs.microsoft.com/oldnewthing/20101018-00/?p=12513
+ * See Part 2 - Now in color
+ * https://devblogs.microsoft.com/oldnewthing/20101019-00/?p=12503
+ * See Part 3 - Apha blended
+ * https://devblogs.microsoft.com/oldnewthing/20101021-00/?p=12483
+ * See Part 4 - Png images
+ * https://devblogs.microsoft.com/oldnewthing/20101022-00/?p=12473
  */
 
 #define WIN32_LEAN_AND_MEAN
@@ -10,9 +18,14 @@
 #pragma once
 #pragma pack(2)
 
-//icon file dir entry
+// icon file dir entry
 typedef struct ICONDIRENTRY_
 {
+    /* The bWidth and bHeight are the dimensions of the image.
+     * Originally, the supported range was 1 through 255, but starting in
+     * Windows 95 (and Windows NT 4), the value 0 is accepted as 
+     * representing a width or height of 256.
+     */
     BYTE        bWidth;          // Width, in pixels, of the image
     BYTE        bHeight;         // Height, in pixels, of the image
     BYTE        bColorCount;     // Number of colors in image (0 if >=8bpp)
@@ -23,7 +36,7 @@ typedef struct ICONDIRENTRY_
     DWORD       dwImageOffset;   // Where in the file is this image?
 } ICONDIRENTRY, *PICONDIRENTRY;
 
-//icon file header
+// icon file header
 typedef struct ICONDIR_
 {
     WORD           idReserved;   // Reserved (must be 0)
@@ -32,15 +45,16 @@ typedef struct ICONDIR_
     ICONDIRENTRY   idEntries[1]; // An entry for each image (idCount of 'em)
 } ICONDIR, *PICONDIR;
 
-//icon file image
+// icon file image
 typedef struct ICONIMAGE_
 {
-    BITMAPINFOHEADER   icHeader;   // DIB header
-    RGBQUAD         icColors[1];   // Color table
-    BYTE            icXOR[1];      // DIB bits for XOR mask
-    BYTE            icAND[1];      // DIB bits for AND mask
+    BITMAPINFOHEADER icHeader;   // DIB header
+    RGBQUAD         icColors[1]; // Color table
+    BYTE            icXOR[1];    // DIB bits for XOR mask
+    BYTE            icAND[1];    // DIB bits for AND mask
 } ICONIMAGE, *PICONIMAGE;
 
+// icon resource group directory entry
 typedef struct GRPICONDIRENTRY_
 {
     BYTE  bWidth;
@@ -50,9 +64,10 @@ typedef struct GRPICONDIRENTRY_
     WORD  wPlanes;
     WORD  wBitCount;
     DWORD dwBytesInRes;
-    WORD  nId;
+    WORD  nId;                   // icon resource id
 } GRPICONDIRENTRY, *PGRPICONDIRENTRY;
 
+// icon resource directory entry
 typedef struct GRPICONDIR_
 {
     WORD idReserved;
