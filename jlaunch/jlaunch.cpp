d931c2ff71eee346c9306719bfef072861097907
Michael Binz <michab66@gmail.com>
2020-03-01 10:50:58 +0100

    Cleanup and testing

--- jlaunch/jlaunch.cpp
+++ jlaunch/jlaunch.cpp
@@ -1,21 +1,31 @@
 /*
  * Java Launcher.
+ * $Id$
  *
  * Copyright (c) 2020 Michael G. Binz
+ *
+ * This is the actual Java process launcher.  A binary version of this
+ * executable is part of JLgen.  JLgen populates the neccessary resources
+ * when it creates the launcher.
+ *
+ * Note that the generated launcher does deliberately only work if its
+ * in the same directory as the jvm.dll to prevent catching a random
+ * jvm.dll in an existing java installation.
  */
 
 #include <array>
-
-#include <tchar.h>
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-#include <jni.h>
 #include <iostream>
 #include <string>
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <tchar.h>
+
+ // Define JAVA_HOME in project include paths if this is not found.
+#include <jni.h>
+
  // Get our resource definitions.
-#include "resource.h"
+#include "jlaunch_resource_ids.h"
 
 // Pick a supported Java version
 #if defined JNI_VERSION_10
@@ -33,59 +43,143 @@ using std::string;
 namespace
 {
 
-int dieWithMessage(const TCHAR* msg)
-{
-    TCHAR szExeFileName[MAX_PATH];
-    GetModuleFileName(NULL, szExeFileName, MAX_PATH);
+    int dieWithMessage(const string& msg)
+    {
+        char szExeFileName[MAX_PATH];
 
-    MessageBox(NULL, msg, szExeFileName, MB_OK | MB_APPLMODAL);
+        GetModuleFileNameA(
+            nullptr,
+            szExeFileName,
+            sizeof(szExeFileName));
 
-    return 1;
-}
+        MessageBoxA(
+            nullptr,
+            msg.c_str(),
+            szExeFileName,
+            MB_OK | MB_APPLMODAL);
 
-/**
- * Read a string resource for a resource id.
- *
- * @param instance The application's instance handle.
- * @param id The resource id.
- * @return The resource string converted to 8bit platform encoding. The
- * string is empty if the resource was not found.
- */
-string getStringResource(HINSTANCE instance, UINT id)
-{
-    WCHAR* notUsed{};
+        return EXIT_FAILURE;
+    }
 
-    // Get the size of the configured resource string.
-    int rcw = LoadStringW(
-        instance,
-        id,
-        // We do not use this since it points to the wide character
-        // version of the resource.
-        (WCHAR*)(&notUsed),
-        0);
+    /**
+     * Read a string resource for a resource id.
+     *
+     * @param instance The application's instance handle.
+     * @param id The resource id.
+     * @return The resource string converted to 8bit platform encoding. The
+     * string is empty if the resource was not found.
+     */
+    string getStringResource(HINSTANCE instance, UINT id)
+    {
+        wchar_t* resourceString =
+            nullptr;
+        void* castHelper =
+            &resourceString;
 
-    // Create our result string in the required size.
-    string result(rcw, '\0');
+        // Get the size of the string. We do not use the
+        // returned pointer, since it points to wide
+        // character data that we do not want to convert.
+        int resourceSizeW = LoadStringW(
+            instance,
+            id,
+            static_cast<LPWSTR>(castHelper),
+            0);
+        if (resourceSizeW == 0)
+            throw std::invalid_argument(
+                "Resource stringW( " + std::to_string(id) + " ) is missing.");
+        // Add one for the terminating NUL.
+        resourceSizeW++;
+        // Create our result string in the required size.
+        std::string result(resourceSizeW, '\0');
 
-    // LoadStringA gets the actual result converted into the
-    // 8bit platform encoding.
-    int rca = LoadStringA(
-        instance,
-        id,
-        // We directly write into the result string.
-        &result[0],
-        rcw + 1);
+        // Now read the actual string converted into the 8bit platform
+        // encoding.
+        int resourceSizeA = LoadStringA(
+            instance,
+            id,
+            &result[0],
+            resourceSizeW);
+        if (resourceSizeA == 0)
+            throw std::invalid_argument(
+                "Unexpected: Resource stringA( " + std::to_string(id) + " ) is missing.");
 
-    if (rcw == rca)
         return result;
+    }
 
-    // TODO(micbinz) implement the error path.
-    DWORD error =
-        GetLastError();
+    int launch(HINSTANCE hInstance)
+    {
+        string mainModule = getStringResource(
+            hInstance,
+            IDS_JAVA_MAIN_MODULE);
 
-    // Return the empty string in case of an error.
-    return string{};
-}
+        // Add the reqired option prefix to set the main module.
+        mainModule =
+            "--add-modules=" +
+            mainModule;
+
+        string mainClassName = getStringResource(
+            hInstance,
+            IDS_JAVA_MAIN_CLASS);
+
+        std::array<JavaVMOption, 1>jvmopt{
+            const_cast<char*>(mainModule.c_str()),
+            nullptr
+        };
+
+        JavaVMInitArgs vmArgs{
+            JNI_VERSION,
+            static_cast<jint>(jvmopt.size()),
+            jvmopt.data(),
+            JNI_FALSE
+        };
+
+        // Create the JVM.
+        JavaVM* javaVM = nullptr;
+        JNIEnv* env = nullptr;
+        long rc = JNI_CreateJavaVM(
+            &javaVM,
+            (void**)&env,
+            &vmArgs);
+        if (rc == JNI_ERR) {
+            throw std::invalid_argument("Error creating VM.");
+        }
+
+        jclass stringClass =
+            env->FindClass("java/lang/String");
+        jclass mainClass =
+            env->FindClass(mainClassName.c_str());
+        if (!mainClass) {
+            string message =
+                "IDS_JAVA_MAIN_CLASS not found: " +
+                mainClassName;
+            throw std::invalid_argument(message);
+        }
+
+        jmethodID mainMethod = env->GetStaticMethodID(
+            mainClass,
+            "main",
+            "([Ljava/lang/String;)V");
+        if (!mainMethod)
+            throw std::invalid_argument("No main() found.");
+
+        jobjectArray argv =
+            env->NewObjectArray(0, stringClass, 0);
+
+        env->CallStaticObjectMethod(
+            mainClass,
+            mainMethod,
+            argv);
+        if (env->ExceptionCheck()) {
+            env->ExceptionDescribe();
+            env->ExceptionClear();
+            throw std::invalid_argument("No good, got exception from main.");
+        }
+
+        // Waits until the VM terminates.
+        javaVM->DestroyJavaVM();
+
+        return EXIT_SUCCESS;
+    }
 
 } // namespace
 
@@ -95,82 +189,14 @@ string getStringResource(HINSTANCE instance, UINT id)
 int APIENTRY wWinMain(
     _In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-    UNREFERENCED_PARAMETER(nCmdShow);
-
-    string mainModule = getStringResource(
-        hInstance,
-        IDS_JAVA_MAIN_MODULE);
-
-    if (mainModule.empty())
-        return dieWithMessage(_T("IDS_JAVA_MAIN_MODULE not in resources."));
-    // Add the reqired option prefix to set the main module.
-    mainModule =
-        "--add-modules=" +
-        mainModule;
-
-    string mainClassName = getStringResource(
-        hInstance,
-        IDS_JAVA_MAIN_CLASS);
-    if (mainClassName.empty())
-        return dieWithMessage(_T("IDS_JAVA_MAIN_CLASS not in resources."));
-
-    std::array<JavaVMOption, 1>jvmopt{
-        const_cast<char*>(mainModule.c_str()),
-        nullptr
-    };
-
-    JavaVMInitArgs vmArgs{
-        JNI_VERSION,
-        static_cast<jint>(jvmopt.size()),
-        jvmopt.data(),
-        JNI_FALSE
-    };
-
-    // Create the JVM.
-    JavaVM* javaVM = nullptr;
-    JNIEnv* env = nullptr;
-    long rc = JNI_CreateJavaVM(
-        &javaVM,
-        (void**)&env,
-        &vmArgs);
-    if (rc == JNI_ERR) {
-        return dieWithMessage(_T("Error creating VM\n. Exiting ..."));
+    try
+    {
+        return launch(hInstance);
     }
-
-    jclass stringClass =
-        env->FindClass("java/lang/String");
-    jclass mainClass =
-        env->FindClass(mainClassName.c_str());
-    if (!mainClass)
-        return dieWithMessage(_T("IDS_JAVA_MAIN_CLASS not found."));
-
-    jmethodID mainMethod = env->GetStaticMethodID(
-        mainClass,
-        "main",
-        "([Ljava/lang/String;)V");
-    if (!mainMethod)
-        return dieWithMessage(_T("No main() found."));
-
-    jobjectArray argv =
-        env->NewObjectArray(0, stringClass, 0);
-
-    env->CallStaticObjectMethod(
-        mainClass,
-        mainMethod,
-        argv);
-    if (env->ExceptionCheck()) {
-        env->ExceptionDescribe();
-        env->ExceptionClear();
-        return dieWithMessage(_T("No good."));
+    catch (std::invalid_argument & e) {
+        return dieWithMessage(e.what());
     }
-
-    // Waits until the VM terminates.
-    javaVM->DestroyJavaVM();
-
-    return 0;
 }
---
+++ jlaunch/jlaunch_resource_ids.h
@@ -0,0 +1,23 @@
+/*
+ * Java Launcher.
+ *
+ * Copyright (c) 2020 Michael G. Binz
+ *
+ * JLaunch resource ids.  These are commonly used by JLgen and JLgen.
+ */
+
+/**
+ * Launcher icon.  This is for Windows Explorer, the
+ * launcher itself does not use this value.
+ */
+#define IDI_ICON1                       1
+
+/**
+ * A string resource that holds the main module name.
+ */
+#define IDS_JAVA_MAIN_MODULE            313
+
+/**
+ * A string resource that holds the main class.
+ */
+#define IDS_JAVA_MAIN_CLASS             314
